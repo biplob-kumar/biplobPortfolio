f3059006e180a92a042c613cd078056668eac783
yourusername <biplobkumar125@gmail.com>
2023-11-27 16:10:22 +0600

    add

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -5,7 +5,7 @@ import { Outlet } from 'react-router-dom';
 import About from '../home/About';
 import Skills from './Skills';
 import Portfolios from './Portfolios';
-import Contactsection from './Contactsection';
+import Contactsection from '../home/Contactsection';
 import Footer from './Footer';
 import Review from '../pages/Review';
 
